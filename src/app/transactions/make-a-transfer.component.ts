03633c61f057687e3c0aa786cd9af7239cf6cf74
Quinntyne Brown <quinntynebrown@gmail.com>
2020-08-03 11:12:26 -0400

    make a transfer form styling

--- src/app/transactions/make-a-transfer.component.ts
+++ src/app/transactions/make-a-transfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-make-a-transfer',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./make-a-transfer.component.css']
 })
 export class MakeATransferComponent implements OnInit {
+
+  public form: FormGroup = new FormGroup({
+    fromAccount: new FormControl(null, []),
+    toAccount: new FormControl(null, []),
+    amount: new FormControl(null, []),
+  });
 
   constructor() { }
 
